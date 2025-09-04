63a4b366e4387410a3a96c9ed7dfa5381357f62b
gizemsangur1 <gizem2002sangur@gmail.com>
2025-09-04 15:44:17 +0300

    (feature) add profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Container, Typography } from "@mui/material";
+
+export default function ProfilePage() {
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Profile
+      </Typography>
+      <Typography>Burada kullanıcının NFT’leri ve DAO geçmişi olacak.</Typography>
+    </Container>
+  );
+}
