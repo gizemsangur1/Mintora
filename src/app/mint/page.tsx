1db8b027db3661541cb495d48c885a1516ca4f2a
gizemsangur1 <gizem2002sangur@gmail.com>
2025-09-04 15:43:29 +0300

    (feature) add mint page

---
+++ src/app/mint/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Container, Typography } from "@mui/material";
+
+export default function MintPage() {
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Mint NFT
+      </Typography>
+      <Typography>Burada NFT minting formu olacak.</Typography>
+    </Container>
+  );
+}
