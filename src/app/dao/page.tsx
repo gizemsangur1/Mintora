65629110d34fd23d84ef045dc1f71bf8bc375521
gizemsangur1 <gizem2002sangur@gmail.com>
2025-09-04 15:43:55 +0300

    (feature) add dao page

---
+++ src/app/dao/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Container, Typography } from "@mui/material";
+
+export default function DaoPage() {
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        DAO Governance
+      </Typography>
+      <Typography>Burada proposal listesi & voting UI olacak.</Typography>
+    </Container>
+  );
+}
