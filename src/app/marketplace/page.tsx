919734d21d8932ddf9d32acc0f37adc0af40ec59
gizemsangur1 <gizem2002sangur@gmail.com>
2025-09-04 15:43:04 +0300

    (feature) add marketplace page

---
+++ src/app/marketplace/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Container, Typography } from "@mui/material";
+
+export default function MarketplacePage() {
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Marketplace
+      </Typography>
+      <Typography>Burada NFT’ler listelenecek.</Typography>
+    </Container>
+  );
+}
